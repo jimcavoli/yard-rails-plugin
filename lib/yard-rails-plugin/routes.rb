30cf35286c8f99f41eb1be7dca019e93d2ba312f
Jim Cavoli <jimcavoli@users.noreply.github.com>
2016-02-02 14:09:46 -0500

    Trying out kramdown...maybe

--- lib/yard-rails-plugin/routes.rb
+++ lib/yard-rails-plugin/routes.rb
@@ -2,11 +2,79 @@ module YARD
   module Rails
     module Plugin
       class Routes
+        class KramdownTableFormatter
+          def initialize
+            @buffer = []
+          end
+    
+          def result
+            @buffer.join("\n")
+          end
+    
+          def section_title(title)
+            @buffer << "\n#{title}:"
+          end
+    
+          def section(routes)
+            @buffer << draw_section(routes)
+          end
+    
+          def header(routes)
+            @buffer << draw_header(routes)
+          end
+    
+          def no_routes(routes)
+            @buffer <<
+            if routes.none?
+              <<-MESSAGE.strip_heredoc
+              You don't have any routes defined!
+              Please add some routes in config/routes.rb.
+              MESSAGE
+            else
+              "No routes were found for this controller"
+            end
+            @buffer << "For more information about routes, see the Rails guide: http://guides.rubyonrails.org/routing.html."
+          end
+    
+          private
+            def draw_section(routes)
+              header_lengths = ['Prefix', 'Verb', 'URI Pattern'].map(&:length)
+              name_width, verb_width, path_width = widths(routes).zip(header_lengths).map(&:max)
+    
+              routes.map do |r|
+                "#{r[:name].rjust(name_width)} | #{r[:verb].ljust(verb_width)} | #{r[:path].ljust(path_width)} | #{r[:reqs]}"
+              end
+            end
+    
+            def draw_header(routes)
+              name_width, verb_width, path_width = widths(routes)
+    
+              "#{"Prefix".rjust(name_width)} | #{"Verb".ljust(verb_width)} | #{"URI Pattern".ljust(path_width)} | Controller#Action"
+            end
+    
+            def widths(routes)
+              [routes.map { |r| r[:name].length }.max || 0,
+               routes.map { |r| r[:verb].length }.max || 0,
+               routes.map { |r| r[:path].length }.max || 0]
+            end
+        end
+        
         def initialize
           puts '[rails-plugin] Analyzing Routes...'
-          ::Rails.application.reload_routes!
-          all_routes = ::Rails.application.routes.routes
-          @routes = all_routes.collect do |route|            
+          all_routes = Rails.application.routes.routes
+          require 'action_dispatch/routing/inspector'
+          @inspector = ActionDispatch::Routing::RoutesInspector.new(all_routes)
+        end
+
+        def generate_routes_description_file(filename)
+          File.open(File.join(Dir.pwd, filename), 'w') do |f|
+            f.puts "# Routes"
+            f.puts @inspector.format(KramdownTableFormatter.new)
+          end
+        end
+
+        def enrich_controllers
+          ::Rails.application.routes.routes.collect do |route|            
             reqs = route.requirements.dup
             rack_app =  route.app.class.name.to_s =~ /^ActionDispatch::Routing/ ? nil : route.app.inspect
             constraints = reqs.except(:controller, :action).empty? ? '' : reqs.except(:controller, :action).inspect
@@ -19,31 +87,7 @@ module YARD
             end
             { name: route.name.to_s, verb: route.verb.to_s, path: route.path,
               controller: controller , action: reqs[:action], rack_app: rack_app, constraints: constraints}
-          end
-        end
-
-        def generate_routes_description_file(filename)
-          File.open(File.join(Dir.pwd, filename), 'w') do |f|
-            f.puts "<h1>Routes</h1><br /><br />"
-            f.puts "<table><tr style='background: #EAF0FF; font-weight: bold; line-height: 28px; text-align: left'><th>&nbsp;&nbsp;Rails Method</th><th>&nbsp;&nbsp;Verb</th><th>&nbsp;&nbsp;Endpoint</th><th>&nbsp;&nbsp;Destionation</th></tr>"
-            i = 0
-            @routes.each do |r|
-              odd_or_even = ((i % 2  == 0) ? 'even' : 'odd')  
-              if r[:rack_app]
-                destination = "<pre>#{r[:rack_app].inspect} #{r[:constraints]}</pre>"
-              else
-                destination = "{#{r[:controller]} #{r[:controller]}}##{r[:action]}  #{r[:constraints]}"
-              end
-              endpoint = r[:path].to_s.gsub(/(:|\*)\w+/) { |m| "<span style='font-family: monospace; color: green'>#{m}</span>"}
-              f.puts "<tr class='#{odd_or_even}'><td>#{r[:name]}</td><td>#{r[:verb]}</td><td>#{endpoint}</td><td>#{destination}</td></tr>"
-              i += 1
-            end
-            f.puts "</table>"
-          end
-        end
-
-        def enrich_controllers
-          @routes.each do |r|
+          end.each do |r|
             if r[:controller] && node = YARD::Registry.resolve(nil, r[:controller], true)
               (node[:routes] ||= []) << r
             end
